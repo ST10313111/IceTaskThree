3af981d5ac9831256b443b2d320feeaf733bed0a
ST10313111 <126805359+ST10313111@users.noreply.github.com>
2025-03-14 12:12:22 +0200

    Abstract class implemented

---
+++ IceTaskThree/Shape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTaskThree
+{
+ public abstract class Shape
+    {
+        public string Name { get; set; }
+
+        public Shape(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"This is a {Name}");
+        }
+
+
+    }
+}
